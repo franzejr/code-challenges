ab0d660f609934df65711ca9be6fd078b92fc684
Franzé Jr. <franzejr@gmail.com>
2015-07-07 11:06:35 -0300

    Update questions.sql

--- sql/questions.sql
+++ sql/questions.sql
@@ -9,3 +9,23 @@ INSERT INTO votes VALUES
 SET @rownum = 0; 
 
 SELECT *,(@rownum := @rownum + 1) AS rank from votes order by votes
+
+
+/*
+2)
+Write a function to capitalize the first letter of a word in a given string;
+Example:   initcap(UNITED   states   Of   AmERIca   )   =   United   States   Of   America
+*/
+
+
+
+DROP FUNCTION IF EXISTS ucfirst;
+ 
+DELIMITER $$
+CREATE FUNCTION ucfirst(str_value TEXT)
+RETURNS TEXT
+BEGIN
+    return CONCAT(ucase(substr(str_value, 1,1)), lcase(substr(str_value, 2)));
+END;
+$$
+DELIMITER ;
